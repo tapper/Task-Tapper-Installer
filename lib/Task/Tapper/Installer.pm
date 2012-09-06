b339952cba4a4287921d8229e5672ce0b0f534df
Steffen Schwigon <ss5@renormalist.net>
2012-09-06 11:55:52 +0200

    initial Tapper bundle

---
+++ lib/Task/Tapper/Installer.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+package Task::Tapper::Installer;
+# ABSTRACT: Tapper - dependencies for machine installer
+
+=pkgroup Tapper
+
+
+=pkg Tapper::Remote
+
+=pkg Tapper::Installer
+
+1;
