527c926c07b411dc603d221aa031ae28f9ee8fc8
Shlomi Fish <shlomif@shlomifish.org>
2012-10-26 17:14:54 +0200

    Add =head1 DESCRIPTION to the POD.
    This is required by some automated tools such as CPAN2Pkg that extract
    the description from it and it is a standard POD thing.

--- lib/Task/Tapper/Installer.pm
+++ lib/Task/Tapper/Installer.pm
@@ -3,6 +3,10 @@ use warnings;
 package Task::Tapper::Installer;
 # ABSTRACT: Tapper - dependencies for machine installer
 
+=head1 DESCRIPTION
+
+This task installs Tapper's dependencies for the machine installer.
+
 =pkgroup Tapper
 
 
